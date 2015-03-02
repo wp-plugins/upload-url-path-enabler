e922fa0bfe682751e1035fd48ed188ceb00ff8be
GregLone <GregLone@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-02 23:44:58 +0000

    Upload Url and Path Enabler 1.0.3: * Small code readability improvements. * Added plugin URL.
    git-svn-id: https://plugins.svn.wordpress.org/upload-url-path-enabler/trunk@1103648 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- readme.txt
+++ readme.txt
@@ -2,12 +2,12 @@
 Contributors: GregLone
 Tags: media, upload, path, url
 Requires at least: 3.5
-Tested up to: 4.0
+Tested up to: 4.1.1
 Stable tag: trunk
 License: GPLv3
 License URI: http://www.screenfeed.fr/gpl-v3.txt
 
-Get the media upload path and url setting fields back in WP 3.5.
+Get the media upload path and url setting fields back in WP 3.5+.
 
 == Description ==
 
@@ -28,6 +28,11 @@ None.
 1. The medias settings page.
 
 == Changelog ==
+
+= 1.0.3 =
+* 2013/03/03
+* Small code readability improvements.
+* Added plugin URL.
 
 = 1.0.2 =
 * 2013/09/14
--- upload-url-path-enabler.php
+++ upload-url-path-enabler.php
@@ -1,44 +1,47 @@
 <?php
 /*
  * Plugin Name: Upload Url and Path Enabler
- * Plugin URI: http://www.screenfeed.fr
+ * Plugin URI: http://www.screenfeed.fr/plugin-wp/upload-url-path-enabler/
  * Description: WordPress 3.5 removes the setting fields to change the media upload path and url. This plugin enable them again. Note that as long as your fields are not empty, you can disable this plugin.
- * Version: 1.0.2
+ * Version: 1.0.3
  * Author: Grégory Viguier
  * Author URI: http://www.screenfeed.fr/greg/
  * License: GPLv3
  * License URI: http://www.screenfeed.fr/gpl-v3.txt
 */
 
-if( !defined( 'ABSPATH' ) )
+if ( ! defined( 'ABSPATH' ) ) {
 	die( 'Cheatin\' uh?' );
+}
 
-global $wp_version;
-if ( version_compare($wp_version, '3.5', '<') || !is_admin() || is_multisite() )
+if ( version_compare( $GLOBALS['wp_version'], '3.5' ) < 0 || ! is_admin() || is_multisite() ) {
 	return;
+}
 
+add_action( 'load-options-media.php', 'uupe_init' );
+add_action( 'load-options.php',       'uupe_init' );
 
-add_action( 'load-options-media.php',	'uupe_init' );
-add_action( 'load-options.php',			'uupe_init' );
 function uupe_init() {
-	if ( !get_option('upload_url_path') && !( get_option('upload_path') != 'wp-content/uploads' && get_option('upload_path') ) ) {
-		register_setting( 'media',	'upload_path',		'esc_attr' );
-		register_setting( 'media',	'upload_url_path',	'esc_url'  );
-		add_settings_field( 'upload_path',		__( 'Store uploads in this folder' ),	'uupe_upload_path',		'media',	'uploads',	array( 'label_for' => 'upload_path' ) );
-		add_settings_field( 'upload_url_path',	__( 'Full URL path to files' ),			'uupe_upload_url_path',	'media',	'uploads',	array( 'label_for' => 'upload_url_path' ) );
+	if ( ! get_option( 'upload_url_path' ) && ! ( get_option( 'upload_path' ) !== 'wp-content/uploads' && get_option( 'upload_path' ) ) ) {
+		register_setting( 'media', 'upload_path',     'esc_attr' );
+		register_setting( 'media', 'upload_url_path', 'esc_url'  );
+		add_settings_field( 'upload_path',     __( 'Store uploads in this folder' ), 'uupe_upload_path',     'media', 'uploads', array( 'label_for' => 'upload_path' )     );
+		add_settings_field( 'upload_url_path', __( 'Full URL path to files' ),       'uupe_upload_url_path', 'media', 'uploads', array( 'label_for' => 'upload_url_path' ) );
 	}
 }
 
 
 function uupe_upload_path( $args ) { ?>
-	<input name="upload_path" type="text" id="upload_path" value="<?php echo esc_attr(get_option('upload_path')); ?>" class="regular-text code" />
-	<p class="description"><?php _e('Default is <code>wp-content/uploads</code>'); ?></p>
+	<input name="upload_path" type="text" id="upload_path" value="<?php echo esc_attr( get_option( 'upload_path' ) ); ?>" class="regular-text code" />
+	<p class="description"><?php _e( 'Default is <code>wp-content/uploads</code>' ); ?></p>
 	<?php
 }
 
 
 function uupe_upload_url_path( $args ) { ?>
-	<input name="upload_url_path" type="text" id="upload_url_path" value="<?php echo esc_attr( get_option('upload_url_path')); ?>" class="regular-text code" />
-	<p class="description"><?php _e('Configuring this is optional. By default, it should be blank.'); ?></p>
+	<input name="upload_url_path" type="text" id="upload_url_path" value="<?php echo esc_attr( get_option( 'upload_url_path' ) ); ?>" class="regular-text code" />
+	<p class="description"><?php _e( 'Configuring this is optional. By default, it should be blank.' ); ?></p>
 	<?php
 }
+
+/**/
